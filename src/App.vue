808a1d1b604e10621cb75f139cff3a43b1c2a53e
domenicolafroscia <domenicolafroscia@gmail.com>
2024-02-01 15:14:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: { AppMain }
+}
+</script>
+
+<template>
+  <h1>Hello world</h1>
+  <AppMain />
+
+</template>
+
+<style lang="scss">
+@use "./style/generals.scss" as *;
+</style>
