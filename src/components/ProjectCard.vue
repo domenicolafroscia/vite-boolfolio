52f9ea5efaf833fa6431aa1a9b27f276546d2b04
domenicolafroscia <domenicolafroscia@gmail.com>
2024-02-02 15:47:55 +0100

    add: store.js implementation

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,11 +1,13 @@
 <script>
+import { store } from '../store';
+
 export default {
     props: {
         project: Object
     },
     data() {
         return {
-            baseUrl: "http://127.0.0.1:8000",
+            store
         }
     },
     computed: {
@@ -21,7 +23,7 @@ export default {
 
 <template>
     <div class="card h-100">
-        <img :src="`${baseUrl}/storage/${project.cover_image}`" alt="" />
+        <img :src="`${store.baseUrl}/storage/${project.cover_image}`" alt="" />
         <div class="card-body">
             <h5>{{ project.title }}</h5>
             <span v-for="technologies in project.technologies" :key="technologies.id"> <span><strong>Technology:</strong> {{ technologies.name }}</span> </span>
