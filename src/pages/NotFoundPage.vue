136d4c9eeece94716419b8d7d5630ac0f33c1de8
domenicolafroscia <domenicolafroscia@gmail.com>
2024-02-05 15:19:44 +0100

    add: not found page implementation

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="text-center">404 NOT FOUND</h1>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/pages/SIngleProject.vue
+++ src/pages/SIngleProject.vue
@@ -15,11 +15,23 @@ export default {
         axios
             .get(`${this.store.baseUrl}/api/projects/${this.$route.params.slug}`)
             .then((resp) => {
-                this.curProject = resp.data.result;
+                if (resp.data.success) {
+                    this.curProject = resp.data.result;
+                } else {
+                    this.$router.push({ name: 'not-found' })
+                }
             })
             .finally(() => {
                 this.loading = false;
             });
+    },
+    computed: {
+        truncateText() {
+            if (this.curProject.content && this.curProject.content.length > 150) {
+                return this.curProject.content.substring(0, 150) + "...";
+            }
+            return this.project.content;
+        },
     },
 };
 </script>
@@ -31,7 +43,16 @@ export default {
             <h3>Loading...</h3>
         </div>
         <div v-else>
-            <h1 class="text-center">{{ curProject.title }}</h1>
+            <h1 class="text-center">Single Project</h1>
+            <div class="card h-100">
+                <img :src="`${store.baseUrl}/storage/${curProject.cover_image}`" alt="" />
+                <div class="card-body">
+                    <h5>{{ curProject.title }}</h5>
+                    <span v-for="technologies in curProject.technologies" :key="technologies.id">
+                        <span><strong>Technology:</strong> {{ technologies.name }}</span> </span>
+                    <p class="my-2">{{ truncateText }}</p>
+                </div>
+            </div>
         </div>
 
     </div>
